5a5ec75601e3dc862ba4072ee30ef8fbcaee27c8
puglb2 <puglb2@gmail.com>
2025-09-09 14:31:20 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  base: './',               // <- keep relative paths in build
+  base: './',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: { entryFileNames: 'widget.js' }
-    }
-  }
+  build: { rollupOptions: { output: { entryFileNames: 'widget.js' } } }
 })
