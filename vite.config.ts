809f5aee1d28a5c747a948ba11f2ecfc57182103
puglb2 <puglb2@gmail.com>
2025-09-09 14:15:53 -0700

    Update and rename vite.config to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: 'widget.js'  // produce a simple bundle name
+        entryFileNames: 'widget.js'
       }
     }
   }
