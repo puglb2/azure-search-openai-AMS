aee275dc59c5e240e4b2947bae484209008331e7
puglb2 <puglb2@gmail.com>
2025-09-09 14:01:03 -0700

    Delete src directory

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-const el = document.getElementById('ams-intake-chat');
-if (el) {
-  const root = createRoot(el);
-  root.render(<App />);
-}
