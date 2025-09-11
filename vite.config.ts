3830c797310d60dad6f174b4ca61ec994c1c4adb
puglb2 <puglb2@gmail.com>
2025-09-11 11:59:53 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: './',                  // ensure relative asset paths
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '/src': fileURLToPath(new URL('./src', import.meta.url)), // if anything still imports "/src"
-    }
-  },
-  build: {
-    rollupOptions: { output: { entryFileNames: 'widget.js' } }
-  }
+  base: './',   // ensures relative asset paths for Azure SWA
+  plugins: [react()]
 })
